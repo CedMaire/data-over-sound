2e164e0cf543a7b125144c46faa4aa26bfdd7559
Alix Jeannerot <alix.jeannerot@epfl.ch>
2018-05-06 14:12:56 +0200

    stragne stuff with the 1500Hz freq

--- src/receiver.py
+++ src/receiver.py
@@ -1,12 +1,1 @@
 # Main receiver program.
-import numpy as np
-import sounddevice as sd
-
-#time : in seconds
-def receive(time):
-    fs=44100
-    sd.default.channels=1
-    record=sd.rec(10*fs,fs,blocking=True) #See if we can't do this in background
-    print(record)
-
-receive(10)
--- src/synth.py
+++ src/synth.py
@@ -5,12 +5,12 @@ import numpy as np
 import sounddevice as sd
 import matplotlib.pyplot as plt
 
+fs = 44100
 # send an array, k bit at a time. It will devide the frequency domain in equal
 # part and send at the divinding frequencies. Produce for the two domain.
 # use time to change in sec the time of the transmission
 def sendBitArray(array, time):
     k = len(array)
-    fs = 44100
     freqs = []
     # calculate the frequencies
     step = 1000/(k+1)
@@ -25,25 +25,29 @@ def sendBitArray(array, time):
         signal = signal + np.sin(2*np.pi*t*f/fs)  # 1st noise
         signal = signal + np.sin(2*np.pi*t*(1000+f)/fs)  # 2nd noise
         print(f)
-    sd.play(signal, fs)
-
+    #sd.play(signal, fs)
     x=np.arange(0,500,1)
     sub=signal[0:500]
     plt.plot(x, sub)
     plt.show()
-    sd.wait()
 
+#TEST
 
+#TEST
+    #sd.wait()
 
 #time : in seconds
 def receiveAndFFT(time):
-    fs=44100
     sd.default.channels=1
-    record=sd.rec(10*fs,fs,blocking=True) #See if we can't do this in background
+    record=sd.rec(time*fs,fs,blocking=True)
+    rfft=np.fft.rfft(signal[0:fs])
+    hz=np.arange(0,fs//2+1)
+    plt.plot(hz,rfft)
+    plt.show()
 
 
 
-# Test
-a = [1, 0, 1, 0, 1]
+#TEST
+a = [0, 0, 1, 0, 0] #apparently, 1500hz doesn't work...
 sendBitArray(a, 2)
-#receive(10)
+#receiveAndFFT(2)
