e00fb8720dbd479bd4d28058ade0d74271dfd50a
Maire Cedric <mairecedric@netplus.ch>
2018-05-23 19:32:59 +0200

    First additions

--- src/synth.py
+++ src/synth.py
@@ -1,7 +1,135 @@
 import numpy as Numpy
 import lib as Lib
+import sounddevice as SoundDevice
+import matplotlib.pyplot as Plot
 
 
 class Synthesizer:
     def __init__(self):
         pass
+
+    def detectNoise(self):
+        record = SoundDevice.rec(Lib.SAMPLES_PER_SEC * Lib.NOISE_DETECTION_TIME,
+                                 Lib.SAMPLES_PER_SEC,
+                                 blocking=True,
+                                 channels=1)[:, 0]
+
+        recordfft = Numpy.fft.fft(record)
+
+        sum1000 = Numpy.sum(Numpy.abs(recordfft[1000:2000]))
+        sum2000 = Numpy.sum(Numpy.abs(recordfft[2000:3000]))
+
+        if (sum1000 > sum2000):
+            print("Noise-free Frequencies: [2000:3000]")
+            return 2
+        else:
+            print("Noise-free Frequencies: [1000:2000]")
+            return 1
+
+    def createWhiteNoise(self):
+        Numpy.random.seed(Lib.NOISE_SEED)
+
+        return Numpy.random.normal(0, 1, Lib.NUMBER_NOISE_SAMPLES)
+
+    def generateCompleteSignal(self, array, nonoise):
+        signal = Numpy.zeros(0)
+        savedSignalDict = {}
+
+        for a in array:
+            if (repr(a) in savedSignalDict):
+                signal = Numpy.concatenate(
+                    [signal, savedSignalDict.get(repr(a))])
+            else:
+                inter = self.generateVectorSignal(a, nonoise)
+                savedSignalDict[repr(a)] = inter
+                signal = Numpy.concatenate([signal, inter])
+
+        return signal
+
+    def generateVectorSignal(self, vector, nonoise):
+        frequencies = []
+
+        for i in range(0, Lib.CHUNK_SIZE):
+            if (vector[i] == 1):
+                if (nonoise == 1):
+                    frequencies.append(
+                        Lib.LOWER_LOW_FREQUENCY_BOUND + Lib.FREQUENCY_STEP * (i + 1))
+                else:
+                    frequencies.append(
+                        Lib.UPPER_LOW_FREQUENCY_BOUND + Lib.FREQUENCY_STEP * (i + 1))
+
+        t = Numpy.arange(Lib.TIME_PER_CHUNK * Lib.SAMPLES_PER_CHUNK)
+        signal = Numpy.zeros(0)
+
+        for f in frequencies:
+            signal = Numpy.concatenate(
+                Numpy.sin(2 * Numpy.pi * t * f / Lib.SAMPLES_PER_CHUNK))
+
+        return signal
+
+    def recordSignal(self):
+        recording = SoundDevice.rec(Lib.RECORDING_SAMPLES_TOTAL,
+                                    Lib.SAMPLES_PER_SEC,
+                                    blocking=True,
+                                    channels=1)[:, 0]
+
+        return recording
+
+    def extractDataSignal(self, record):
+        noiseToSyncOn = self.createWhiteNoise()
+
+        maxDotProduct = 0
+        index = 0
+
+        for i in range(record.size - (Lib.NUMBER_DATA_SAMPLES + Lib.NUMBER_NOISE_SAMPLES)):
+            dotProduct = Numpy.dot(noiseToSyncOn,
+                                   record[i:Lib.NUMBER_NOISE_SAMPLES + i])
+            if (dotProduct > maxDotProduct):
+                maxDotProduct = dotProduct
+                index = i
+
+        begin = index + Lib.NUMBER_NOISE_SAMPLES
+        end = begin + Lib.NUMBER_DATA_SAMPLES
+
+        return record[begin:end]
+
+    def decodeSignalToBitVectors(self, signal, nonoise):
+        chunks = [signal[i:i + Lib.SAMPLES_PER_CHUNK]
+                  for i in range(0, len(signal), Lib.SAMPLES_PER_CHUNK)]
+
+        bitVectors = []
+        for chunk in chunks:
+            bitVectors.append(
+                self.decodeSignalChunkToBitVector(chunk, nonoise))
+
+        return bitVectors
+
+    def decodeSignalChunkToBitVector(self, chunk, nonoise):
+        w = Numpy.abs(Numpy.imag(Numpy.fft.fft(chunk)))
+        f = Numpy.abs(Numpy.fft.fftfreq(len(w), 1 / Lib.SAMPLES_PER_SEC))
+
+        Plot.plot(f, w)
+        Plot.show()
+
+        '''
+        peaks = np.empty(2*ones)
+        i = 0
+        for x in range(2*ones):
+            idx = np.argmax(np.abs(w))
+            freq = f[idx]
+            freq_in_hertz = abs(freq * frequence)
+            peaks[i]=freq_in_hertz
+            w = np.delete(w, idx)
+            idx = np.argmax(np.abs(w))
+            w = np.delete(w, idx)
+            i+=1
+        peaks=np.sort(peaks)
+        return peaks
+        '''
+
+
+if __name__ == "__main__":
+    synthesizer = Synthesizer()
+
+    synthesizer.decodeSignalChunkToBitVector(
+        Numpy.sin(2 * Numpy.pi * Numpy.arange(Lib.SAMPLES_PER_CHUNK) * 1500 / Lib.SAMPLES_PER_SEC), 1)
